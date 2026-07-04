4a7f93e0a93abf62cdbe20e1810d885172b7ed9b
EricMwangi <ericmwas01@gmail.com>
2026-07-04 09:42:04 +0300

    Add an spsc queue
    Fifo1 is a single consumer single producer fifo
    for asynchronous application.
    NB: It is not a multithreaded application
    
    Signed-off-by: EricMwangi <ericmwas01@gmail.com>

---
+++ spsc/spsc.cc
@@ -0,0 +1,15 @@
+#include "spsc.hpp"
+#include <print>
+
+// erors to include 
+// -werror -wextra -wconversion address sanitizer undefined behaviour santizer 
+// thread sanitizer 
+// writing tests -gtest 
+// performance testing 
+// add values from one thread 
+//read the values from another thread 
+
+int main()
+{
+    std::println("SPSC starting...."); 
+}
---
+++ spsc/spsc.hpp
@@ -0,0 +1,50 @@
+#include <memory>
+
+template <typename T, typename Alloc = std::allocator<T>> 
+class Fifo1 : private Alloc 
+{
+    std::size_t capacity_; 
+    T* ring_; 
+    std::size_t pushCursor_{}; 
+    std::size_t popCursor_{}; 
+
+
+    public: 
+        explicit Fifo1(std::size_t capacity, Alloc const& alloc = Alloc{}) 
+            : Alloc(alloc), capacity_(capacity), ring_{std::allocator_traits::allocate(*this, capacity)}
+        {}
+        ~Fifo1(){
+            while(not empty()){
+                ring_[popCursor_ % capacity_].~T(); 
+                ++popCursor_; 
+            }
+            std::allocator_traits::deallocate(*this, ring_, capacity_); 
+        }
+        auto capacity() const {return capacity_;}
+        auto size() const {return pushCursor_ - popCursor; };
+        auto empty() const { return size() == 0; }
+        auto full() const {return size() == capacity(); }
+
+        auto push(T const& value)
+        {
+            if(full()){
+                return false; 
+            }
+            new(&ring_[pushCursor_ % capacity_]) T(value); 
+            ++pushCursor_; 
+            return true; 
+        }
+        auto pop(T* value)
+        {
+            if(empty())
+            {
+                return false; 
+            }
+            value = ring_[popCursor_ % capacity_]; 
+            ring_[popCursor_ % capacity_].~T(); 
+            ++popCursor_; 
+            return true; 
+        }
+
+}; 
+
