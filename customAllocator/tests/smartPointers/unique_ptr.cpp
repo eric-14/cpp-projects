215fe156e955101d28fdcc9ce1fa65fe20823605
EricMwangi <ericmwas01@gmail.com>
2026-07-19 12:55:55 +0300

    Add: custom allocation functionality
    Implementing both C++11 and C++17 based pmr and custom allocators
    PMR uniquely makes it intuitive to handle custom allocation across large software systems
    From trialling C++11 implementation using allocator_traits and allocator combined with the embeddding allocation type
    increases the complexity of customizing allocation.
    PMR handles all the boilerplate however, the current alleged limitation is use of vpointer table inefficiency,
    which is debunked by John Lakos cppcon talk on allocators. The perfomance cost of vpointer is negligible compared to using global allocator
    
    Signed-off-by: EricMwangi <ericmwas01@gmail.com>

---
+++ customAllocator/tests/smartPointers/unique_ptr.cpp
@@ -0,0 +1,81 @@
+template<typename T>
+class unique_ptr {
+    private: 
+        T* m_ptr = nullptr; 
+    public: 
+        explicit unique_ptr(T *_obj): 
+            m_ptr{_obj} {}
+        ~unique_ptr()
+        {
+            delete m_ptr; 
+        }
+        
+        T* get() const {return m_ptr;}; 
+        T* operator*() const {return *m_ptr}; 
+        T* operator->() const {return m_ptr}; 
+
+        //delete copy constructor 
+        unique_ptr(const unique_ptr&) = delete; 
+        //delete copy assignment constructor 
+        unique_ptr& operator=(const unique_ptr&) = delete; 
+
+        explicit operator bool() const {
+            if(m_ptr != nullptr) return true; 
+            return false; 
+        }
+
+        //move constructor 
+        unique_ptr(unique_ptr &&other) noexcept {
+            m_ptr = other.m_ptr; 
+            other.m_ptr = nullptr; 
+
+        }
+        unique_ptr& operator=(unique_ptr &&other) noexcept
+        {
+            if(this != &other)
+            {
+                delete m_ptr; 
+                m_ptr = other.m_ptr; 
+                other.m_ptr = nullptr; 
+            }
+            return *this; 
+        }
+        T* release() noexcept
+        {
+            // get the addr of the obj
+            T* temp = m_ptr; 
+            m_ptr = nullptr; 
+            return temp; 
+        }
+        void reset(T *other = nullptr)
+        {
+            
+                //delete the old obj
+                delete m_ptr; 
+                m_ptr = other; 
+        }
+        void swap(unique_ptr& other)
+        {
+            T* tmp = other.m_ptr; 
+            other.m_ptr = m_ptr; 
+            m_ptr = tmp;
+        }
+
+}; 
+
+
+/**
+ * TODO: 
+ * 
+ * - Add Custom deleter 
+ * - Array support 
+ * - Store pointer + deleter together 
+ * - support nullptr constructor 
+ * - Support conversion from Derived base to base 
+ * - comparison hash 
+ * - get deleter() returntthe default deleter 
+ * - make_unique
+ * 
+ * 
+ * 
+ */
